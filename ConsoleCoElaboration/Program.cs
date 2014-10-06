c9701bebdb61abac354a89dbdb9d3a3dbe4f6eae
Jubacabra <jubacabra@live.dk>
2014-10-06 13:29:12 +0200

    Added Visual Studio Project

---
+++ ConsoleCoElaboration/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCoElaboration
+{
+    class Program
+    {
+        /// <summary>
+        /// This is the main method
+        /// it receives a input from commandline
+        /// and checks if its numbers or letter
+        /// it will then check if it's a palindrome
+        /// if it is a number it will present the same number in binary or roman 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            IsPalindrome()
+        }
+
+        /// <summary>
+        /// returns true if input is a Palindrome
+        /// else false
+        /// </summary>
+        /// <param name="text">text input to be validated</param>
+        /// <returns></returns>
+        private static bool IsPalindrome(String text)
+        {
+        }
+        /// <summary>
+        /// Returns true if input is only letters
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static bool ContainsOnlyLetters(String text)
+
+
+        private static bool ConstainsOnlyDigits(String text)
+
+        private static bool ConvertToBinaryNumbers(int input)
+
+        private static string ConvertToRomanNumbers(String text)
+    }
+
+}
